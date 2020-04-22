b63337460d793416bea71c8533022ec1522b592b
drdenke <nikoladenkedenic@gmail.com>
2020-04-22 12:40:31 +0200

    change name of class to ParseURL

--- src/com/hyperskill/ParseURL.java
+++ src/com/hyperskill/ParseURL.java
@@ -2,7 +2,7 @@ package com.hyperskill;
 
 import java.util.Scanner;
 
-public class Main {
+public class ParseURL {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
